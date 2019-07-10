a2a763b9129b1c1698bd2a7a459e810052a94ad7
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-07-10 21:48:47 +0100

    add test for node info parsing

--- tests/test_fsnavigator_map.py
+++ tests/test_fsnavigator_map.py
@@ -5,7 +5,8 @@ from fsnavigator_map import FsNavigatorMap
 class TestFsNavigatorMap(FsNavigatorMap):
     def _read_file(self):
         return [
-            'PESUL	40.881944	-8.115000	14	W2	    L	PRT	    41.273000	-8.687833	0	    Y	VIS	    40.723417	-7.885833	9500	Y'
+            'PESUL	40.881944	-8.115000	14	W2	    L	PRT	    41.273000	-8.687833	0	    Y	VIS	    40.723417	-7.885833	9500	Y',
+            'ODLIX	38.678889	-9.317222	14	Y207	B	LIS	    38.887750	-9.162806	9500	N	EKMAR	38.557500	-9.521389	9500	Y'
         ]
 
 class TestParseAirwayNode(TestCase):
@@ -13,6 +14,16 @@ class TestParseAirwayNode(TestCase):
         self.map = TestFsNavigatorMap()
 
     def test_canParseNodeInfo(self):
-        info = self.map.nodes['PESUL']
-        self.assertEqual(info.x, 40.881944)
-        self.assertEqual(info.y, -8.115000)
+        node = self.map.nodes['PESUL']
+        self.assertEqual(node.x, 40.881944)
+        self.assertEqual(node.y, -8.115000)
+        self.assertEqual(node.via, 'W2')
+        self.assertEqual(node.via_type, 'L')
+
+        node = self.map.nodes['ODLIX']
+        self.assertEqual(node.x, 38.678889)
+        self.assertEqual(node.y, -9.317222)
+        self.assertEqual(node.via, 'Y207')
+        self.assertEqual(node.via_type, 'B')
+
+    def test_canParseNeighboursForNode(self):
