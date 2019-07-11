e92a7cea338fa4bf22a2d21b6143c00005e06ff6
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-07-11 11:51:05 +0100

    add route map build command to cli

--- cli.py
+++ cli.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python
 import webbrowser
 from time import time
+import pickle
 import click
 
 from fsnavigator_map import FsNavigatorMap
 from route_finder import RouteFinder
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def build():
+    stats = time()
+    map = FsNavigatorMap()
+    stats = time() - stats
+    click.echo(f'Route map built in {stats:.1f}s.')
+    with open('.map', 'wb') as file:
+        pickle.dump(map, file)
+
+def get_map():
+    try:
+        with open('.map', 'rb') as file:
+            return pickle.load(file)
+    except TypeError:
+        return FsNavigatorMap()
+
+@cli.command()
 @click.argument('start')
 @click.argument('end')
-def route(start, end):
-    click.echo('Loading route map...')
-    _start = time()
-    map = FsNavigatorMap()
-    _end = time()
-    click.echo(f'Route map loaded in {_end - _start}s')
+@click.option('--open/--no-open', '-o/-no', default=False)
+def route(start, end, open):
+    map = get_map()
 
     start = map.nodes[start]
     end = map.nodes[end]
     route = RouteFinder(map=map, start=start, end=end)
 
-    click.echo(f'Finding route from {start.name} to {end.name}...')
-    _start = time()
+    stats = time()
     route.find()
-    _end = time()
-    click.echo(f'Found route: {route.atc_route} in {_end - _start}s.')
-    click.echo(f'Opening SkyVector https://skyvector.com/?fpl={route.atc_route}')
+    stats = time() - stats
+    click.echo(f'Found route in {stats:.1f}s.')
+    click.echo(f'ROUTE: {route.atc_route}')
     webbrowser.open(f'https://skyvector.com/?fpl={route.atc_route}')
 
 if __name__ == '__main__':
-    route()
+    cli()
